d4c85802f12ff53cfe88595e1aa2d6c54c47c97b
Donnaya <donnayawang@gmail.com>
2021-05-13 14:57:54 +0700

    [EDIT] index.js

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 //version ใหม่
 const express = require("express");
-const path = require("path");
+const app = express();
+//const path = require("path");
+const cors = require("cors");
+const mariadb = require("mariadb");
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-const pool = require("./db");
+app.use(cors());
+app.use(express.json());
+
+const pool = mariadb.createPool({
+  host: "adorminis.cp1aphjaifpb.ap-southeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "adorminis",
+  database: "adorminis",
+  connectionLimit: 5,
+});
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+
+  pool.query(
+    "INSERT INTO RESIDENT (EMAIL, PASSWORD, FNAME, LNAME) VALUES (?,?,?,?)",
+    [email, password, fname, lname],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted"); //send message
+      }
+    }
+  );
+});
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+// app.get("/api", (req, res) => {
+//   res.json({ message: "Hello from server!" });
+// });
 
 // All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
