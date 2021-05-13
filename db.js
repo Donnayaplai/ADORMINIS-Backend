29fb4adc6c1c0f638e0eafe34446a4101cc5d749
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-05-13 14:46:21 +0700

    [UPDATE] db.js

--- db.js
+++ db.js
@@ -12,11 +12,11 @@ async function asyncFunction() {
     conn = await pool.getConnection();
     const rows = await conn.query("SELECT 1 as val");
     console.log(rows); //[ {val: 1}, meta: ... ]
-    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
-      1,
-      "mariadb",
-    ]);
-    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+    // const res = await conn.query("INSERT INTO myTable value (?, ?)", [
+    //   1,
+    //   "mariadb",
+    // ]);
+    // console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
   } catch (err) {
     console.log(err);
   } finally {
