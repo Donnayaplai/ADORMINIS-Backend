183f65706d71590fdd004655a19add83148d007e
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-05-13 18:12:18 +0700

    [ADD] dormsetting model

---
+++ model/dormsetting.js
@@ -0,0 +1,35 @@
+const pool = require("../db");
+const dormsettingQueries = require("../queries/dormsetting");
+
+const getElectricityPrice = async (dormID) => {
+  const electricityPrice = await pool.query(dormsettingQueries.GET_ELECTRICITYPRICE(dormID));
+  return electricityPrice;
+};
+
+const getwaterPrice = async (dormID) => {
+    const waterPrice = await pool.query(dormsettingQueries.GET_WATERPRICE(dormID));
+    return waterPrice;
+  };
+
+  const getMinWaterUnit = async (dormID) => {
+    const minWaterUnit = await pool.query(dormsettingQueries.GET_MINWATERUNIT(dormID));
+    return minWaterUnit;
+  };
+
+  const addSetting = async (waterPrice, electricityPrice, minWaterUnit, multprepaid, dormID) => {
+    const Setting = await pool.query(dormsettingQueries.ADD_SETTING(waterPrice, electricityPrice, minWaterUnit, multprepaid, dormID));
+    return Setting;
+  };
+
+  const editSetting = async (waterPrice, electricityPrice, minWaterUnit, multprepaid, dormID) => {
+    const newSetting = await pool.query(dormsettingQueries.EDIT_SETTING(waterPrice, electricityPrice, minWaterUnit, multprepaid, dormID));
+    return newSetting;
+  };
+
+  const dormsettingModel = (module.exports = {
+    getElectricityPrice,
+    getwaterPrice,
+    getMinWaterUnit,
+    addSetting,
+    editSetting
+  });
