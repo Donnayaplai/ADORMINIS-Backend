527be7d16f147b42c9a67df365ad589bbeb0b353
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-05-20 15:46:38 +0700

    [UPDATE] model utility class form

--- model/Utility.js
+++ model/Utility.js
@@ -1,37 +1,65 @@
 const pool = require("../db");
 const utilityQueries = require("../queries/utility");
 
-const getWaterMeterNo = async (roomID) => {
-  const getWaterMeterNo = await pool.query(
-    utilityQueries.GET_WATER_METERNO(roomID)
-  );
-  return getWaterMeterNo;
-};
+class utility {
+  constructor() {
+    this.utility;
+  }
 
-const getElectricityMeterNo = async (roomID) => {
-  const getElectricityMeterNo = await pool.query(
-    utilityQueries.GET_ELECTRICITY_METERNO(roomID)
-  );
-  return getElectricityMeterNo;
-};
+  async getWaterMeterNo(roomID) {
+    this.user = await pool.query(utilityQueries.GET_WATER_METERNO(roomID));
+    return this.utility;
+  }
 
-const addWaterMeterNo = async (waterNo, meterDate, roomID) => {
-  const addWaterMeterNo = await pool.query(
-    utilityQueries.ADD_WATER_METERNO(waterNo, meterDate, roomID)
-  );
-  return addWaterMeterNo;
-};
+  async getElectricityMeterNo(roomID) {
+    this.user = await pool.query(utilityQueries.GET_ELECTRICITY_METERNO(roomID));
+    return this.utility;
+  }
 
-const addElectricityMeterNo = async (waterNo, meterDate, roomID) => {
-  const addElectricityMeterNo = await pool.query(
-    utilityQueries.ADD_ELECTRICITY_METERNO(waterNo, meterDate, roomID)
-  );
-  return addElectricityMeterNo;
-};
+  async addWaterMeterNo(waterNo, meterDate, roomID) {
+    this.user = await pool.query(utilityQueries.ADD_WATER_METERNO(waterNo, meterDate, roomID));
+    return this.utility;
+  }
 
-const utilityModel = (module.exports = {
-  getWaterMeterNo,
-  getElectricityMeterNo,
-  addWaterMeterNo,
-  addElectricityMeterNo,
-});
+  async addElectricityMeterNo(waterNo, meterDate, roomID) {
+    this.user = await pool.query(utilityQueries.ADD_ELECTRICITY_METERNO(waterNo, meterDate, roomID));
+    return this.utility;
+  }
+}
+
+module.exports = utility;
+
+//   const getWaterMeterNo = async (roomID) => {
+//     const getWaterMeterNo = await pool.query(
+//       utilityQueries.GET_WATER_METERNO(roomID)
+//     );
+//     return getWaterMeterNo;
+//   };
+
+//   const getElectricityMeterNo = async (roomID) => {
+//     const getElectricityMeterNo = await pool.query(
+//       utilityQueries.GET_ELECTRICITY_METERNO(roomID)
+//     );
+//     return getElectricityMeterNo;
+//   };
+
+//   const addWaterMeterNo = async (waterNo, meterDate, roomID) => {
+//     const addWaterMeterNo = await pool.query(
+//       utilityQueries.ADD_WATER_METERNO(waterNo, meterDate, roomID)
+//     );
+//     return addWaterMeterNo;
+//   };
+
+//   const addElectricityMeterNo = async (waterNo, meterDate, roomID) => {
+//     const addElectricityMeterNo = await pool.query(
+//       utilityQueries.ADD_ELECTRICITY_METERNO(waterNo, meterDate, roomID)
+//     );
+//     return addElectricityMeterNo;
+//   };
+
+// const utilityModel = (module.exports = {
+//   getWaterMeterNo,
+//   getElectricityMeterNo,
+//   addWaterMeterNo,
+//   addElectricityMeterNo
+// });
